205083e1cea69172d568902b348b311f73c5d05c
mustafacoban96 <mustafa.coban@std.hku.edu.tr>
2025-01-21 22:00:45 +0300

    wrap app with auth-provider and router-provider

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,13 +1,21 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App';
+import { RouterProvider } from 'react-router';
+import router from './router';
+import { AuthProvider } from './context/AuthContext';
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+  // <React.StrictMode>
+  //   <App />
+  // </React.StrictMode>
+
+  <AuthProvider>
+    <RouterProvider router={router}/>
+  </AuthProvider>
+
+  
 );
 
