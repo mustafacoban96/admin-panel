eebb658328177a234e332803f7d6bf713237820e
mustafacoban96 <mustafa.coban@std.hku.edu.tr>
2025-01-21 14:54:17 +0300

    login controller and register controller jwt integration

--- api/Controller/AccountController.cs
+++ api/Controller/AccountController.cs
@@ -3,9 +3,11 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using api.Dtos.Account;
+using api.Interfaces;
 using api.Models;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 
 namespace api.Controller
 {
@@ -14,10 +16,15 @@ namespace api.Controller
     public class AccountController : ControllerBase
     {
         private readonly UserManager<AppUser> _userManager;
+        private readonly ITokenService _tokenService;
+        private readonly SignInManager<AppUser> _signinManager;
+
         
-        public AccountController(UserManager<AppUser> userManager)
+        public AccountController(UserManager<AppUser> userManager, ITokenService tokenService, SignInManager<AppUser> signinManager)
         {
             _userManager = userManager;
+            _tokenService = tokenService;
+            _signinManager = signinManager;
         }
 
         [HttpPost("register")]
@@ -38,7 +45,11 @@ namespace api.Controller
                 if(createdUser.Succeeded){
                     var roleResult = await _userManager.AddToRoleAsync(appUser,"User");
                     if(roleResult.Succeeded){
-                        return Ok("User created");
+                         return Ok(new NewUserDto{
+                            UserName = appUser.UserName,
+                            Email = appUser.Email,
+                            Token = _tokenService.CreateToken(appUser)
+                        });
                     }else{
                         return StatusCode(500, roleResult.Errors);
                     }
@@ -53,5 +64,30 @@ namespace api.Controller
                 return StatusCode(500, e);
             }
         }
+
+        [HttpPost("login")]
+        public async Task<IActionResult> Login([FromBody] LoginDto loginDto)
+        {
+            if(!ModelState.IsValid)
+                return BadRequest(ModelState);
+
+            
+            var user = await _userManager.Users.FirstOrDefaultAsync(x => x.UserName == loginDto.Username.ToLower());
+
+            if(user == null) return Unauthorized("Invalid username!");
+
+            var result = await _signinManager.CheckPasswordSignInAsync(user,loginDto.Password,false);
+
+            if(!result.Succeeded) return Unauthorized("Username not found and/or password incorrect");
+
+            return Ok(
+                new NewUserDto{
+                    UserName = user.UserName,
+                    Email = user.Email,
+                    Token = _tokenService.CreateToken(user)
+                }
+            );
+        }
+
     }
 }
