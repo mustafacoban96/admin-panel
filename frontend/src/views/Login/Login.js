2d5c572d7c523c7f7e9dbe0fba0c97f8bcf2fc8c
mustafacoban96 <mustafa.coban@std.hku.edu.tr>
2025-01-22 15:25:22 +0300

    yup validation login-register

--- frontend/src/views/Login/Login.js
+++ frontend/src/views/Login/Login.js
@@ -1,90 +1,102 @@
-import React, { useRef } from 'react'
+import React from 'react';
 import logo from "../../assets/SANKOLogo-02.png";
 import useAuthService from '../../services/auth-service';
 import { ToastContainer } from 'react-toastify';
+import { useForm } from "react-hook-form";
+import { yupResolver } from "@hookform/resolvers/yup";
+import { loginSchema } from '../../validations/LoginScheme'; 
+
 
 const Login = () => {
+  const { loginApi } = useAuthService();
 
-  const {login} = useAuthService();
-  const usernameRef = useRef();
-  const passwordRef = useRef();
+  // react-hook-form setup
+  const {
+    register:login,// it connects inputs to form
+    handleSubmit, // if all validations are successful , run callback
+    formState: { errors }, // formState includes form errors
+  } = useForm({
+    resolver: yupResolver(loginSchema),// validation rules
+  });
 
-
-  const handleLogin = (e) => {
-    e.preventDefault(); // Sayfanın yeniden yüklenmesini engeller
-
-    // Referanslardan değerleri al
-    const username = usernameRef.current.value;
-    const password = passwordRef.current.value;
-
-    const payload = {
-      username: username,
-      password: password,
-    }
-
-    console.log("Username:", username);
-    console.log("Password:", password);
-
-    // Burada API çağrısı veya doğrulama işlemini gerçekleştirebilirsiniz
-    login(payload)
-    
+  // Handle form submission
+  const onSubmit = (data) => {
+    //console.log("Form Data:", data);
+    // API call
+    loginApi(data); 
   };
 
   return (
     <>
-    <div className="flex items-center justify-center min-h-screen bg-gray-100">
-    <div className="bg-white p-8 rounded-lg shadow-lg w-full max-w-md">
-      <div className='flex justify-center'> 
-          <img src={logo} className='w-48' alt='sanko-logo'/>
-      </div>
-      <h2 className="text-3xl font-bold text-center text-gray-700 mb-6">Sanko Panel</h2>
-      <form onSubmit={handleLogin} className="space-y-4">
-        <div>
-          <label
-            htmlFor="username"
-            className="block text-sm font-medium text-gray-600"
-          >
-            Username
-          </label>
-          <input
-            type="text"
-            id="username"
-            ref={usernameRef}
-            className="w-full mt-1 px-4 py-2 border border-gray-300 rounded-lg focus:ring-2 focus:ring-blue-500 focus:outline-none"
-            placeholder="Enter your username"
-          />
-        </div>
-        
-        <div>
-          <label
-            htmlFor="password"
-            className="block text-sm font-medium text-gray-600"
-          >
-            Password
-          </label>
-          <input
-            type="password"
-            id="password"
-            ref={passwordRef}
-            className="w-full mt-1 px-4 py-2 border border-gray-300 rounded-lg focus:ring-2 focus:ring-blue-500 focus:outline-none"
-            placeholder="Enter your password"
-          />
-        </div>
-        
-        <div className='flex justify-center py-5'>
-          <button
-            type="submit"
-            className="w-72 bg-blue-600 text-white py-2 px-4 rounded-lg hover:bg-blue-700 focus:ring-2 focus:ring-blue-500 focus:outline-none"
-          >
-            Login
-          </button>
-        </div>
-      </form>
-    </div>
-</div>
-<ToastContainer/>
-</>
-  )
-}
+      <div className="flex items-center justify-center min-h-screen bg-gray-100">
+        <div className="bg-white p-8 rounded-lg shadow-lg w-full max-w-md">
+          <div className="flex justify-center">
+            <img src={logo} className="w-48" alt="sanko-logo" />
+          </div>
+          <h2 className="text-3xl font-bold text-center text-gray-700 mb-6">
+            Sanko Panel
+          </h2>
+          <form onSubmit={handleSubmit(onSubmit)} className="space-y-4">
+            <div>
+              <label
+                htmlFor="username"
+                className="block text-sm font-medium text-gray-600"
+              >
+                Username
+              </label>
+              <input
+                type="text"
+                id="username"
+                {...login("username")}
+                className={`w-full mt-1 px-4 py-2 border ${
+                  errors.username ? "border-red-500" : "border-gray-300"
+                } rounded-lg focus:ring-2 focus:ring-blue-500 focus:outline-none`}
+                placeholder="Enter your username"
+              />
+              {errors.username && (
+                <p className="text-red-500 text-sm mt-1">
+                  {errors.username.message}
+                </p>
+              )}
+            </div>
 
-export default Login
+            <div>
+              <label
+                htmlFor="password"
+                className="block text-sm font-medium text-gray-600"
+              >
+                Password
+              </label>
+              <input
+                type="password"
+                id="password"
+                {...login("password")}
+                className={`w-full mt-1 px-4 py-2 border ${
+                  errors.password ? "border-red-500" : "border-gray-300"
+                } rounded-lg focus:ring-2 focus:ring-blue-500 focus:outline-none`}
+                placeholder="Enter your password"
+              />
+              {errors.password && (
+                <p className="text-red-500 text-sm mt-1">
+                  {errors.password.message}
+                </p>
+              )}
+            </div>
+
+            <div className="flex justify-center py-5">
+              <button
+                type="submit"
+                className="w-72 bg-blue-600 text-white py-2 px-4 rounded-lg hover:bg-blue-700 focus:ring-2 focus:ring-blue-500 focus:outline-none"
+              >
+                Login
+              </button>
+            </div>
+          </form>
+        </div>
+      </div>
+      <ToastContainer />
+    </>
+  );
+};
+
+export default Login;
--- frontend/src/views/Register/Register.js
+++ frontend/src/views/Register/Register.js
@@ -1,80 +1,130 @@
-import React from 'react'
+import React from 'react';
+import { useForm } from 'react-hook-form';
+import { yupResolver } from '@hookform/resolvers/yup';
 import logo from "../../assets/SANKOLogo-02.png";
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
+import { registerSchema } from '../../validations/RegisterScheme';
+import useAuthService from '../../services/auth-service';
+
+
 const Register = () => {
+  //api
+  const {registerApi} = useAuthService();
+  const {
+    register:register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm({
+    resolver: yupResolver(registerSchema),
+  });
+
+  const onSubmit = (data) => {
+    // API call simulation
+    console.log('Form Data:', data);
+    registerApi(data);
+  };
+
   return (
-    <div className="flex items-center justify-center min-h-screen bg-gray-100">
-    <div className="bg-white p-8 rounded-lg shadow-lg w-full max-w-md">
-      <div className='flex justify-center'> 
-                <img src={logo} className='w-48' alt='sanko-logo'/>
+    <>
+      <div className="flex items-center justify-center min-h-screen bg-gray-100">
+        <div className="bg-white p-8 rounded-lg shadow-lg w-full max-w-md">
+          <div className="flex justify-center">
+            <img src={logo} className="w-48" alt="sanko-logo" />
+          </div>
+          <h2 className="text-3xl font-bold text-center text-gray-700 mb-6">Sanko Panel</h2>
+          <form onSubmit={handleSubmit(onSubmit)} className="space-y-4">
+            <div>
+              <label
+                htmlFor="username"
+                className="block text-sm font-medium text-gray-600"
+              >
+                Username
+              </label>
+              <input
+                type="text"
+                id="username"
+                {...register('username')}
+                className={`w-full mt-1 px-4 py-2 border ${
+                  errors.username
+                    ? 'border-red-500'
+                    : 'border-gray-300'
+                } rounded-lg focus:ring-2 focus:ring-blue-500 focus:outline-none`}
+                placeholder="Enter your username"
+              />
+              <p className="text-red-500 text-sm mt-1">{errors.username?.message}</p>
             </div>
-      <h2 className="text-3xl font-bold text-center text-gray-700 mb-6">Sanko Panel</h2>
-      <form className="space-y-4">
-        <div>
-          <label
-            htmlFor="username"
-            className="block text-sm font-medium text-gray-600"
-          >
-            Username
-          </label>
-          <input
-            type="text"
-            id="username"
-            className="w-full mt-1 px-4 py-2 border border-gray-300 rounded-lg focus:ring-2 focus:ring-blue-500 focus:outline-none"
-            placeholder="Enter your username"
-          />
+            <div>
+              <label htmlFor="email" className="block text-sm font-medium text-gray-600">
+                Email
+              </label>
+              <input
+                type="email"
+                id="email"
+                {...register('email')}
+                className={`w-full mt-1 px-4 py-2 border ${
+                  errors.email
+                    ? 'border-red-500'
+                    : 'border-gray-300'
+                } rounded-lg focus:ring-2 focus:ring-blue-500 focus:outline-none`}
+                placeholder="Enter your email"
+              />
+              <p className="text-red-500 text-sm mt-1">{errors.email?.message}</p>
+            </div>
+            <div>
+              <label
+                htmlFor="password"
+                className="block text-sm font-medium text-gray-600"
+              >
+                Password
+              </label>
+              <input
+                type="password"
+                id="password"
+                {...register('password')}
+                className={`w-full mt-1 px-4 py-2 border ${
+                  errors.password
+                    ? 'border-red-500'
+                    : 'border-gray-300'
+                } rounded-lg focus:ring-2 focus:ring-blue-500 focus:outline-none`}
+                placeholder="Enter your password"
+              />
+              <p className="text-red-500 text-sm mt-1">{errors.password?.message}</p>
+            </div>
+            <div>
+              <label
+                htmlFor="confirm_password"
+                className="block text-sm font-medium text-gray-600"
+              >
+                Confirm Password
+              </label>
+              <input
+                type="password"
+                id="confirm_password"
+                {...register('confirm_password')}
+                className={`w-full mt-1 px-4 py-2 border ${
+                  errors.confirm_password
+                    ? 'border-red-500'
+                    : 'border-gray-300'
+                } rounded-lg focus:ring-2 focus:ring-blue-500 focus:outline-none`}
+                placeholder="Confirm your password"
+              />
+              <p className="text-red-500 text-sm mt-1">{errors.confirm_password?.message}</p>
+            </div>
+            <div className="flex justify-center py-5">
+              <button
+                type="submit"
+                className="w-72 bg-blue-600 text-white py-2 px-4 rounded-lg hover:bg-blue-700 focus:ring-2 focus:ring-blue-500 focus:outline-none"
+              >
+                Register
+              </button>
+            </div>
+          </form>
         </div>
-        <div>
-          <label htmlFor="email" className="block text-sm font-medium text-gray-600">
-            Email
-          </label>
-          <input
-            type="email"
-            id="email"
-            className="w-full mt-1 px-4 py-2 border border-gray-300 rounded-lg focus:ring-2 focus:ring-blue-500 focus:outline-none"
-            placeholder="Enter your email"
-          />
-        </div>
-        <div>
-          <label
-            htmlFor="password"
-            className="block text-sm font-medium text-gray-600"
-          >
-            Password
-          </label>
-          <input
-            type="password"
-            id="password"
-            className="w-full mt-1 px-4 py-2 border border-gray-300 rounded-lg focus:ring-2 focus:ring-blue-500 focus:outline-none"
-            placeholder="Enter your password"
-          />
-        </div>
-        <div>
-          <label
-            htmlFor="password"
-            className="block text-sm font-medium text-gray-600"
-          >
-            Confirm Password
-          </label>
-          <input
-            type="password"
-            id="confirm_password"
-            className="w-full mt-1 px-4 py-2 border border-gray-300 rounded-lg focus:ring-2 focus:ring-blue-500 focus:outline-none"
-            placeholder="Enter your password"
-          />
-        </div>
-        <div className='flex justify-center py-5'>
-          <button
-            type="submit"
-            className="w-72 bg-blue-600 text-white py-2 px-4 rounded-lg hover:bg-blue-700 focus:ring-2 focus:ring-blue-500 focus:outline-none"
-          >
-            Register
-          </button>
-        </div>
-      </form>
-    </div>
-</div>
+      </div>
+      <ToastContainer />
+    </>
+  );
+};
 
-  )
-}
-
-export default Register
+export default Register;
