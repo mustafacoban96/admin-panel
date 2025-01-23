e19ded7b746db2d9cc47829519020949ce26eeee
mustafacoban96 <mustafa.coban@std.hku.edu.tr>
2025-01-23 09:17:16 +0300

    dark and light mode tailwindcss

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -1,13 +1,26 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: ["./src/**/*.{html,js}"],
+  darkMode:'class',
   theme: {
     extend: {
       colors:{
-        // background color
-        appGray: '#f3f4f6',
-        txtColor:'#374151',
-        txtTitle:'#1f2937'
+        // dark
+        darkBack:'#0f172a', //gray-900
+        darkTxtTitle:'#e5e7eb',
+        dIcon:'#e5e7eb',
+        dText:'#9ca3af',
+        dMainBack:'#1e293b',
+        //light
+        lightBack:'#f5f5f4',// sidebar ve header için b-stone-100
+        lIcon:'#991b1b',
+        ltxtTitle:'#1f2937',
+        lText:'#4b5563',
+        lMainBack:'#e5e7eb',
+        ////////
+        
+        
+       
       }
     },
   },
