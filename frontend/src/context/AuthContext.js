1edc3741d98c79302434d053b3e12fa16c62d1e5
mustafacoban96 <mustafa.coban@std.hku.edu.tr>
2025-01-27 15:07:44 +0300

    jwt encode structure

--- frontend/src/context/AuthContext.js
+++ frontend/src/context/AuthContext.js
@@ -1,4 +1,7 @@
 import React, {  createContext, useContext, useEffect, useState } from 'react'
+import useAuthService from '../services/auth-service';
+import { jwtDecode } from "jwt-decode";
+import { Bounce, toast } from "react-toastify";
 
 
 
@@ -13,15 +16,75 @@ const AuthContext = createContext({
 export const AuthProvider = ({children}) =>{
     const [user, _setUser] = useState(null);
     const [token, _setToken] = useState(null);
+    const {logoutApi} = useAuthService();
+    const [checkToken,setCheckToken] = useState(false);
+    
+   
+    
 
     useEffect(() => {
-        // LocalStorage'dan verileri başlangıçta çekiyoruz.
-        const storedUser = localStorage.getItem('USER');
-        const storedToken = localStorage.getItem('ACCESS_TOKEN');
-    
+        const storedUser = localStorage.getItem("USER");
+        const storedToken = localStorage.getItem("ACCESS_TOKEN");
+
         if (storedUser) _setUser(JSON.parse(storedUser));
-        if (storedToken) _setToken(storedToken);
-      }, []);
+        if (storedToken) {
+            _setToken(storedToken);
+           
+        }
+    }, []);
+
+    useEffect(() => {
+        if (!token) return;
+
+        const checkTokenExpiry = () => {
+           try {
+            const decodedToken = jwtDecode(token);
+            const expirationTime = decodedToken.exp 
+            const currentTime = Math.floor(Date.now() / 1000); //saniye cinsinden
+            console.log("dec",expirationTime);
+            console.log("cur",currentTime);
+            console.log(currentTime - expirationTime)
+            console.log(currentTime <= expirationTime)
+            if(expirationTime <= currentTime){
+                setCheckToken(true);
+            }
+           } catch (error) {
+                console.error("Token çözümleme hatası:", error);
+           }
+        };
+        const intervalId = setInterval(checkTokenExpiry, 21600000); // 21600 sn = 5 saat
+        if(checkToken){
+            toast.warning("Oturum süreniz doldu, 20 saniye sonra oturum sonlanacaktır.", {
+                position: "top-right",
+                autoClose: 20000,
+                hideProgressBar: false,
+                closeOnClick: true,
+                pauseOnHover: true,
+                draggable: true,
+                theme: "light",
+                transition: Bounce,
+            });
+            const clearLocalStorage = () => {
+                return new Promise((resolve) => {
+                    localStorage.removeItem("USER");
+                    localStorage.removeItem("ACCESS_TOKEN");
+                    setUser(null);
+                    setToken(null);
+                    resolve();
+                   });
+            }
+            setTimeout(() => {
+                clearLocalStorage().then(() =>{
+                    window.location.reload();
+                }); // Oturumu kapat
+                 // Sayfayı yeniden yükle
+            }, 20000); // 20 saniye (20000 milisaniye)
+            clearInterval(intervalId);
+        }
+        return () => clearInterval(intervalId);
+        
+    }, [token,checkToken,logoutApi]);
+
 
 
     const setToken = (token) =>{
@@ -29,6 +92,7 @@ export const AuthProvider = ({children}) =>{
 
         if(token){
             localStorage.setItem('ACCESS_TOKEN',token)
+            //monitorTokenExpiration(token);
         }
         else{
             localStorage.removeItem('ACCESS_TOKEN')
